eb948f03ce5bfaa994e07f3cd45e1ebab2b84a2a
Logan Bonney <wakeup2early@gmail.com>
2023-04-23 01:51:50 -0600

    Updated image generation to use dalle

--- config.js
+++ config.js
@@ -3,6 +3,5 @@ require("dotenv").config();
 module.exports = {
 	token: process.env.TOKEN,
   clientId: process.env.CLIENT_ID,
-  imgBBKey: process.env.IMG_BB_KEY,
   openaiKey: process.env.OPENAI_KEY,
 };
